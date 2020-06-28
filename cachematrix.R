607c6af410e6a58494fc7d5fe297058a727f7fc5
larilla15 <67537117+larilla15@users.noreply.github.com>
2020-06-28 16:36:58 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -38,25 +38,34 @@ cacheSolve <- function(x, ...) {
         matrix_inv
 }
 ##Now we test the two functions. Let's create the object my_matrix which contains the function makeCacheMatrix and a matrix (numbers from 1 to 4, 2 rows, 2 columns)
+
 my_matrix<-makeCacheMatrix(matrix(1:4,2,2))
+
 ## with the argument my_matrix$get() we print the matrix
+
 my_matrix$get()
   ##   [,1] [,2]
 ##[1,]    1    3
 ##[2,]    2    4
 ## Let's find the inverse
+
 my_matrix$getInv()
-NULL  #the result is NULL.  First we have to compute the inverse with the second function
+## NULL 
+
+#the result is NULL.  First we have to compute the inverse with the second function
+
 cacheSolve(my_matrix)
  ##   [,1] [,2]
 ##[1,]   -2  1.5
 ##[2,]    1 -0.5 
 ##This is the inverse matrix. If we want, now we can use getInv and check the matrix
+
 my_matrix$getInv()
  ##    [,1] [,2]
 ##[1,]   -2  1.5
 ##[2,]    1 -0.5 
 ## The inverse matrix is the same as the previous one
+
 ## Now we can set a different matrix (numbers from 4 to 7, 2 rows, 2 columns)
 my_matrix$set(matrix(c(4:7), 2, 2))
 my_matrix$get()
@@ -64,9 +73,11 @@ my_matrix$get()
 ##[1,]    4    6
 ##[2,]    5    7
 ## This is our new matrix
+
 my_matrix$getInv()
 ## NULL 
 ## As before we have to run the cacheSolve function to get the inverse matrix
+
 cacheSolve(my_matrix)
   ##   [,1] [,2]
 ##[1,] -3.5    3
@@ -75,6 +86,7 @@ my_matrix$getInv()
 ##[,1] [,2]
 ##[1,] -3.5    3
 ##[2,]  2.5   -2
+
 ## Now we set another matrix: number 1 to 16, 4 rows, 4 columns)
 my_matrix$set(matrix(1:16,4,4))
 my_matrix$get()
@@ -84,7 +96,7 @@ my_matrix$get()
 ## [3,]    3    7   11   15
 ## [4,]    4    8   12   16
 ##If we run the cacheSolve function for this new matrix, we get an error
-cacheSolve(my_matrix)
- Error in solve.default(data, ...) : 
-  Lapack routine dgesv: system is exactly singular: U[3,3] = 0 
+## cacheSolve(my_matrix)
+ ## Error in solve.default(data, ...) : 
+  ## Lapack routine dgesv: system is exactly singular: U[3,3] = 0 
 ## The new matrix is singular, therefore we cannot compute the inverse matrix.
